d4074d726009d98b7d9c87c3d7df122d5791afdb
Thinkaboutmin <19597109+Thinkaboutmin@users.noreply.github.com>
2020-09-16 17:10:35 -0300

    Exercicio 1 - Jomar

---
+++ src/jomar/exc1/Main.kt
@@ -0,0 +1,6 @@
+package jomar.exc1
+
+fun main() {
+    val exercicio1 = Exercicio1()
+    exercicio1.imprimirMapas()
+}
--- src/jomar/exc1/exc1.kt
+++ src/jomar/exc1/exc1.kt
@@ -1,5 +1,46 @@
 package jomar.exc1
 
-fun exercicio1() {
+class Exercicio1 {
+    val loteriaDosSonhos = mapOf(
+        0 to "Ovos",
+        1 to "Água",
+        2 to "Escopeta",
+        3 to "Cavalo",
+        4 to "Dentista",
+        5 to "Fogo"
+    )
 
+    val apelidos = mapOf(
+        "João" to arrayOf("Juan", "Fissura", "Maromba"),
+        "Miguel" to arrayOf("Night Watch", "Bruce Wayne", "Tampinha"),
+        "Maria" to arrayOf("Wonder Woman", "Mary", "Marilene"),
+        "Lucas" to arrayOf("Lukinha", "Jorge", "George")
+    )
+
+    fun imprimirMapas() {
+        println("Loteria dos sonhos:")
+        for (key in loteriaDosSonhos.keys) {
+            println("$key: ${loteriaDosSonhos[key]}")
+        }
+
+        println("\nApelidos")
+        for (key in apelidos.keys) {
+
+            val apelidosFormatado = this.formatarApelidos(apelidos[key])
+            println("$key tem os seguintes apelidos $apelidosFormatado")
+        }
+    }
+
+    private fun formatarApelidos(apelidos: Array<String>?): String {
+        var apelidosStr = ""
+
+        if (apelidos != null) {
+            for (apelido in apelidos) {
+                apelidosStr += "${apelido}, "
+            }
+            apelidosStr = apelidosStr.removeSuffix(", ")
+        }
+
+        return apelidosStr
+    }
 }
